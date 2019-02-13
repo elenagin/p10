326222138aa9421e0f01f2a4146f2fdbfe53fa95
Elena Gin <32081397+lennygin@users.noreply.github.com>
2019-02-13 16:48:39 -0600

    Update ecuacion.java

--- ecuacion.java
+++ ecuacion.java
@@ -1,19 +1,13 @@
 public class ecuacion {
-    private float area;
+    private double area;
+    private double delta = 0.005;
 
-    public void setArea(int grado, float a, float b, float c, float d, int l1, int l2) {
-        float delta = (l2 - l1) / 6;
-        float suma = 0;
-
-
-        for (int i = 0; i < 6; i++) {
-            float first_mid_p = a + (delta / 2) + i * (b - a) / 6;
-            suma += (first_mid_p * first_mid_p - first_mid_p + 3);
-        }
-        area = suma * delta;
+    public void setArea(double a, double b, double c, double d, int l1, int l2) {
+        for (float i = l1; i < l2; i += delta)
+            area += (a * i * i * i) + (b * i * i) + (c * i) + d;
     }
 
-    public float getArea() {
-        return area;
+    public double getArea() {
+        return area * delta;
     }
 }
