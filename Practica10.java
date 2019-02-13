7be45357f27360b33dfdd73bcc564cfddee9d77f
Elena Gin <32081397+lennygin@users.noreply.github.com>
2019-02-13 16:48:16 -0600

    Update Practica10.java

--- Practica10.java
+++ Practica10.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Practica10 {
     public static void main(String args[]) {
         int grado;
-        float a = 0, b = 0, c = 0, d = 0;
+        double a = 0, b = 0, c = 0, d = 0;
         int l1, l2;
         Scanner entrada = new Scanner(System.in);
         ecuacion eq = new ecuacion();
@@ -16,25 +17,24 @@ public class Practica10 {
         for (int i = grado; i >= 0; i--) {
             System.out.println("Ingresa el coeficiente para el grado " + i + " de x:");
             if (i == 3) {
-                a = entrada.nextFloat();
+                a = entrada.nextDouble();
             } else if (i == 2) {
-                b = entrada.nextFloat();
+                b = entrada.nextDouble();
             } else if (i == 1) {
-                c = entrada.nextFloat();
+                c = entrada.nextDouble();
             } else if (i == 0) {
-                d = entrada.nextFloat();
+                d = entrada.nextDouble();
             }
         }
+        System.out.println("\nIngresa los límites de integración:\na= ");
+        l1 = entrada.nextInt();
+        System.out.println("\nb= ");
+        l2 = entrada.nextInt();
 
-        do {
-            System.out.print("\nIngresa los límites de integración:\na= ");
-            l1 = entrada.nextInt();
-            System.out.print("\nb= ");
-            l2 = entrada.nextInt();
-        } while (l1 < 0 && l2 > 10);
+        eq.setArea(a, b, c, d, l1, l2);
+        //JOptionPane.showMessageDialog(null, "Area:\n"+ eq.getArea());
 
-        eq.setArea(grado, a, b, c, d, l1, l2);
-        System.out.println("Area = " + eq.getArea());
+        System.out.println("Area = " );
 
     }
 }
