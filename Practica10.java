bbcb04cb42158bfa00af9948dd0d9610aa357aea
Elena Gin <32081397+lennygin@users.noreply.github.com>
2019-02-13 15:20:32 -0600

    Update Practica10.java

--- Practica10.java
+++ Practica10.java
@@ -4,11 +4,12 @@ public class Practica10 {
     public static void main(String args[]) {
         int grado;
         float a = 0, b = 0, c = 0, d = 0;
+        int l1, l2;
         Scanner entrada = new Scanner(System.in);
         ecuacion eq = new ecuacion();
 
         do {
-            System.out.println("Ingresa el grado de la ecuación:");
+            System.out.println("ECUACIÓN\nIngresa el grado de la ecuación:");
             grado = entrada.nextInt();
         } while (grado > 3 || grado < 1);
 
@@ -24,7 +25,15 @@ public class Practica10 {
                 d = entrada.nextFloat();
             }
         }
-        eq.setArea(grado, a, b, c, d);
+
+        do {
+            System.out.print("\nIngresa los límites de integración:\na= ");
+            l1 = entrada.nextInt();
+            System.out.print("\nb= ");
+            l2 = entrada.nextInt();
+        } while (l1 < 0 && l2 > 10);
+
+        eq.setArea(grado, a, b, c, d, l1, l2);
         System.out.println("Area = " + eq.getArea());
 
     }
